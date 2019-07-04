95c9b03a331cb7f2d552655f56aaed747826c2f5
Akshay Suresh <akshay_s@rollingarrays.com>
2019-07-05 00:27:52 +0530

    ADDED: jest and basic test

--- test/goals.test.js
+++ test/goals.test.js
@@ -3,14 +3,21 @@ const app = require('../app');
 
 describe('Testing routes', () => {
   
-  test('GET base path', done => {
-    
+  it.only('GET base path', done => {
     return request(app).get('/')
       .then(response => {
         expect(response.statusCode).toBe(200);
+        expect(typeof response.body).toBe('object');
+        expect(response.body).toHaveProperty('message');
         done();
       });
+  });
 
-  })
+  it('checks something 1', () => {})
 
-})
+  it('checks something 2', () => {})
+
+});
+
+// http://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/
+// https://jestjs.io/docs/en/testing-frameworks
